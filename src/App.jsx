0d0b827b0eab694f311c3a57bf020a014565869a
harsh sharma <165451996+harshdev2909@users.noreply.github.com>
2025-03-23 21:00:04 +0530

    changes

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import './App.css'
+import { TokenLaunchpad } from './components/TokenLaunchpad'
+import React, { FC, useMemo } from 'react';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
+import {
+    WalletModalProvider,
+    WalletDisconnectButton,
+    WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
+
+// Default styles that can be overridden by your app
+import '@solana/wallet-adapter-react-ui/styles.css';
+function App() {
+  return (
+    <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+    <WalletProvider wallets={[]} autoConnect>
+        <WalletModalProvider>
+
+          <div style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+           padding: 20,
+        }}>
+            <WalletMultiButton />
+            <WalletDisconnectButton />
+          </div>
+
+            <TokenLaunchpad/>
+        </WalletModalProvider>
+    </WalletProvider>
+</ConnectionProvider>
+  )
+}
+
+export default App
