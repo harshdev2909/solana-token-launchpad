409b4d731e37f705386be28b2e4751e528324655
harsh sharma <165451996+harshdev2909@users.noreply.github.com>
2025-03-23 21:32:02 +0530

    polyfills

--- src/components/TokenLaunchpad.jsx
+++ src/components/TokenLaunchpad.jsx
@@ -1,5 +1,14 @@
-
+import { createMint } from '@solana/spl-token'
 export function TokenLaunchpad() {
+
+    function createToken(){
+        const name = document.getElementById('name').value
+        const symbol = document.getElementById('symbol').value
+        const image = document.getElementById('image').value
+        const supply = document.getElementById('supply').value
+        console.log(name, symbol, image, supply)
+    }
+
     return  <div style={{
         height: '100vh',
         display: 'flex',
@@ -8,10 +17,10 @@ export function TokenLaunchpad() {
         flexDirection: 'column'
     }}>
         <h1>Solana Token Launchpad</h1>
-        <input className='inputText' type='text' placeholder='Name'></input> <br />
-        <input className='inputText' type='text' placeholder='Symbol'></input> <br />
-        <input className='inputText' type='text' placeholder='Image URL'></input> <br />
-        <input className='inputText' type='text' placeholder='Initial Supply'></input> <br />
-        <button className='btn'>Create a token</button>
+        <input className='inputText' type='text' placeholder='Name' id="name"></input> <br />
+        <input className='inputText' type='text' placeholder='Symbol' id="symbol"></input> <br />
+        <input className='inputText' type='text' placeholder='Image URL' id="image"></input> <br />
+        <input className='inputText' type='text' placeholder='Initial Supply'id="supply"></input> <br />
+        <button className='btn' onClick={createToken}>Create a token</button>
     </div>
 }
